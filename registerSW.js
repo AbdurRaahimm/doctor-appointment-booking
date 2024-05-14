cc7907a67c7149f66b43b37c1cbaef1e3f96a09f
“AbdurRaahimm” <rahim703936@gmail.com>
2024-05-14 19:13:36 +0600

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/doctor-appointment-booking/sw.js', { scope: '/doctor-appointment-booking/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
